070737a0ca09d3d670fd57b35c338438235cb74c
Ivan Mashintsev <mashintsev@gmail.com>
2012-02-07 09:42:07 +0400

    Ext 1.1.1 patch

--- src/js/Ext.ux.js
+++ src/js/Ext.ux.js
@@ -288,3 +288,57 @@ Ext.override(Ext.plugins.PullRefreshPlugin, {
 		return true;
 	}
 });
+
+var version = Ext.platformVersionDetail;
+if(version.major == 1 && version.minor == 0 && version.patch == 3) {
+	Ext.override(Ext.util.Stateful, {
+		set: function(fieldName, value) {
+			var me = this,
+				fields = me.fields,
+				modified = me.modified,
+				convertFields = [],
+				currentValue = me[me.persistanceProperty][fieldName],
+				field, key, i;
+			
+			
+			if (arguments.length == 1 && Ext.isObject(fieldName)) {
+				for (key in fieldName) {
+					if (!fieldName.hasOwnProperty(key)) {
+						continue;
+					}
+					
+					
+					
+					field = fields.get(key);
+					if (field && field.convert !== field.type.convert) {
+						convertFields.push(key);
+						continue;
+					}
+					
+					me.set(key, fieldName[key]);
+				}
+				
+				for (i = 0; i < convertFields.length; i++) {
+					field = convertFields[i];
+					me.set(field, fieldName[field]);
+				}
+				
+			} else {
+				if (fields) {
+					field = fields.get(fieldName);
+					
+					if (field && field.convert) {
+						value = field.convert(value, me);
+					}
+				}
+				
+				me[me.persistanceProperty][fieldName] = value;
+	
+				me.dirty = true;
+				if (!me.editing) {
+					me.afterEdit();
+				}
+			}
+		}
+	});
+}
